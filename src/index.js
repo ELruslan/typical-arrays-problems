48e19260148a07a8d565f29475c3dbdce5426533
elruslan <eloyan666@gmail.com>
2021-08-19 23:13:01 +0300

    Complite

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  result=0
+  for(i=0;i<array.length;i++)
+  if(array[i]<0){
+    result= Math.min.apply(null, array)
+  }return result;
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  for(i=0;i<array.length;i++)
+  if(array[i]>0){
+    result= Math.max.apply(null, array)}
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  for(i=0;i<array.length;i++)
+  if(array[i]>0){
+    result= array.reduce((a,b) => a + b, 0) / array.length}
+  return result;
 }
